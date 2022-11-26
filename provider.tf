15f6e336a2aa4b6002632f11d32ac9f4c382a983
amza simpore <amzasimpore@gmail.com>
2022-11-26 11:13:21 -0600

    fixing provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "remote" {
+    organization = "ibt-learning"
+    workspaces {
+      name = "ibt-terraform-infra"
+    }
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-west-2"
-  access_key = "<replace with your access key>"
-  secret_key = "<replace with your secret key>"
+  access_key = "AKIA2A4DHKVA2EJS4IV7"
+secret_key = "k2pp57VqRqTHMA90S+wwCDdz4ngZc4MV16D7pqIO"
 }
-
